c1775d9da5926dcaea2ddb2e926949990b31e1ec
BigPorojects <djamilaagro@gmail.com>
2026-08-27 17:02:21 +0100

    Create lesson2.js

---
+++ 07-content/lesson-00001/lesson2.js
@@ -0,0 +1,8 @@
+import title01 from './title-01/title01.js';
+import title02 from './title-02/title02.js';
+
+// تجميع وتصدير كافة عناوين هذا الدرس في مصفوفة واحدة
+export default [
+  title01,
+  title02
+];
