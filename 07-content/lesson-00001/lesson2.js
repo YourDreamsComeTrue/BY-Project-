73ea22798945a47dc4afb44c92334c67a63c6ab0
BigPorojects <djamilaagro@gmail.com>
2026-08-27 17:25:05 +0100

    Update lesson2.js

--- 07-content/lesson-00001/lesson2.js
+++ 07-content/lesson-00001/lesson2.js
@@ -1,8 +1,26 @@
-import title01 from './title-01/title01.js';
-import title02 from './title-02/title02.js';
+// 07-content/lesson-00001/lesson2.js
 
-// تجميع وتصدير كافة عناوين هذا الدرس في مصفوفة واحدة
-export default [
-  title01,
-  title02
-];
+async function loadAvailableTitles() {
+  const titles = [];
+
+  // محاولة تحميل العنوان الأول
+  try {
+    const t1 = await import('./title-01/title01.js');
+    titles.push(t1.default);
+  } catch (e) {
+    console.warn("العنوان 1 غير جاهز بعد");
+  }
+
+  // محاولة تحميل العنوان الثاني
+  try {
+    const t2 = await import('./title-02/title02.js');
+    titles.push(t2.default);
+  } catch (e) {
+    console.warn("العنوان 2 غير جاهز بعد");
+  }
+
+  return titles;
+}
+
+export default await loadAvailableTitles();
+  
